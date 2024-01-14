22e013dc8c36630384d6fa538659d5e3656a9939
uhahaha2000 <43199041+uhahaha2000@users.noreply.github.com>
2024-01-14 19:14:50 +0900

    chore: tailwind-merge 추가

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,15 +1,38 @@
 import type { Metadata } from "next";
+import { twMerge } from "tailwind-merge";
 
 export const metadata: Metadata = {
   title: "tailwind test",
   description: "=)",
 };
 
+function MyGenericInput(props: any) {
+  //const className = `border rounded bg-red-200 px-2 py-1 ${props.className || ""}`;
+  const className = twMerge(
+    "border rounded px-2 py-1 text-orange-200 bg-green-400 border-0",
+    props.className,
+  );
+  return <input {...props} className={className} />;
+}
+
+function MySlightlyModifiedInput(props: any) {
+  return (
+    <MyGenericInput {...props} className="bg-gray-400 p-3 text-blue-600" />
+  );
+}
+
 export default function test() {
   return (
-    <div className="h-[100vh] flex-1 bg-black bg-yellow-300 text-blue-700 text-red-600">
-      으캬캬캬컁
-      <h1 className="flex h-5 w-full bg-slate-200 bg-white text-base font-medium"></h1>
+    <div className="bg-blac h-[100vh] flex-1 bg-yellow-300 font-sans text-red-600">
+      <h1 className="mb-6 flex h-5 w-full text-2xl font-medium">
+        요기요기 테스트
+      </h1>
+      <div className="my-3">
+        기본 : <MyGenericInput />
+      </div>
+      <div>
+        수정 : <MySlightlyModifiedInput />
+      </div>
     </div>
   );
 }
