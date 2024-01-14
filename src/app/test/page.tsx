ed75c66ef2dd880485ee6176a1ce011ca3ad6f4e
uhahaha2000 <43199041+uhahaha2000@users.noreply.github.com>
2024-01-14 16:58:15 +0900

    chore: prettier 추가

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,10 +1,15 @@
-import type { Metadata } from 'next'
+import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: 'tailwind test',
-  description: '=)',
-}
+  title: "tailwind test",
+  description: "=)",
+};
 
 export default function test() {
-  return <p>레이아웃</p>;
+  return (
+    <div className="h-[100vh] flex-1 bg-black bg-yellow-300 text-blue-700 text-red-600">
+      으캬캬캬컁
+      <h1 className="flex h-5 w-full bg-slate-200 bg-white text-base font-medium"></h1>
+    </div>
+  );
 }
