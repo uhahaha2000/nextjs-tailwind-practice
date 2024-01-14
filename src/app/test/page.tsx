32646dafd18de3795b57603f2ae3d60e223fef85
uhahaha2000 <43199041+uhahaha2000@users.noreply.github.com>
2024-01-15 00:20:08 +0900

    ex add

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -21,18 +21,48 @@ function MySlightlyModifiedInput(props: any) {
   );
 }
 
+const BUTTON_VARIANTS = {
+  primary: "h-10 w-full bg-blue-500 hover:bg-blue-600 text-white",
+  secondary: "h-10 w-full bg-gray-500 hover:bg-gray-600 text-white",
+  danger: "h-10 w-full bg-red-500 hover:bg-red-600 text-white",
+};
+
+function ButtonType(props: any) {
+  const className = `${props.className}`;
+  return (
+    <button type="button" className={className}>
+      {props.text}
+    </button>
+  );
+}
+
+const MyComponent = ({ isIos, text }) => {
+  // isIos 값에 따라 동적으로 클래스를 결정
+  const dynamicClasses = isIos
+    ? "text-center text-blue-500 bg-red-300"
+    : "text-left text-gray-500 bg-blue-300";
+
+  return <p className={`my-3 p-4 ${dynamicClasses}`}>{text} 이예요</p>;
+};
+
 export default function test() {
   return (
-    <div className="bg-blac h-[100vh] flex-1 bg-yellow-300 font-sans text-red-600">
+    <div className="min-h-[100vh] flex-1 bg-yellow-300 font-sans text-red-600">
       <h1 className="mb-6 flex h-5 w-full text-2xl font-medium">
         요기요기 테스트
       </h1>
       <div className="my-3">
         기본 : <MyGenericInput />
       </div>
-      <div>
+      <div className="mb-5">
         수정 : <MySlightlyModifiedInput />
       </div>
+
+      <ButtonType className={BUTTON_VARIANTS.primary} text="버튼이지용1" />
+      <ButtonType className={BUTTON_VARIANTS.secondary} text="버튼이지용2" />
+      <ButtonType className={BUTTON_VARIANTS.danger} text="버튼이지용3" />
+
+      <MyComponent isIos={true} text="아이폰" />
     </div>
   );
 }
