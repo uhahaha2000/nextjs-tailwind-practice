4afd7dd3115c8dfecd4316c410bfbbe910e47cf3
uhahaha2000 <43199041+uhahaha2000@users.noreply.github.com>
2024-01-14 15:53:33 +0900

    wrap추가

---
+++ src/app/test/page.tsx
@@ -0,0 +1,10 @@
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'tailwind test',
+  description: '=)',
+}
+
+export default function test() {
+  return <p>레이아웃</p>;
+}
